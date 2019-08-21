a983aac71335f945f5a9fa327b7af617674b952c
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-08-21 09:16:58 -0400

    pull

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express ();
 //Port
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 //___________________
 //Database
