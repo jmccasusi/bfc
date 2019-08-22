cf47d945b9723076b7129ca0c2e555b72bcc7d80
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-22 08:42:26 -0400

    commented out urlencoded extended false

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use('/babies', BabyController);
 app.use('/users', UsersController);
 app.use('/sessions', SessionsController);
